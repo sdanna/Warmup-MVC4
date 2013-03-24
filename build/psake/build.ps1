f4c7e23e3841a54f3dca91ca8c3599d66847b327
Steven D'Anna <steven.danna@gmail.com>
2013-03-24 14:07:43 -0500

    Adding xunit to the psake build script

--- build/psake/build.ps1
+++ build/psake/build.ps1
@@ -1,21 +1,26 @@
-properties {
-  $testMessage = 'Executed Test!'
-  $compileMessage = 'Executed Compile!'
-  $cleanMessage = 'Executed Clean!'
+properties {  
+  $projectName = '__NAME__'
+  $configuration = 'Release'  
 }
 
 task default -depends Compile
 
 task Clean { 
-  msbuild ../../src/__NAME__.sln /target:Clean
+	exec {
+		msbuild ../../src/$projectName.sln /target:Clean /v:quiet
+	}
 }
 
 task Compile -depends Clean { 
-  msbuild ../../src/__NAME__.sln /p:Configuration="Debug"
+	exec { 
+		msbuild ../../src/$projectName.sln /p:Configuration="$configuration" /v:quiet
+	}
 }
 
 task Test -depends Compile, Clean { 
-
+	exec {
+		../../tools/xunit-1.9.1/xunit.console.clr4.exe ../../src/$projectName.UnitTests/bin/$configuration/$projectName.UnitTests.dll /nunit results.xml
+	}
 }
 
 
