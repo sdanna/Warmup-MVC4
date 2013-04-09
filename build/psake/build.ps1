d1f7073386e7837cd1dbce8bb47ad28d8c4c1768
Steven D'Anna <steven.danna@gmail.com>
2013-04-08 21:02:04 -0500

    Cleaning up the psake build script.

--- build/psake/build.ps1
+++ build/psake/build.ps1
@@ -1,25 +1,28 @@
 properties {  
   $projectName = '__NAME__'
   $configuration = 'Release'  
+  $solutionFile = resolve-path ../../src/$projectName.sln
+  $xunitToolsPath = resolve-path ../../tools/xunit-1.9.1/xunit.console.clr4.exe
+  $unitTestProject = resolve-path ../../src/$projectName.UnitTests/bin/$configuration/$projectName.UnitTests.dll
 }
 
 task default -depends Compile
 
 task Clean { 
 	exec {
-		msbuild ../../src/$projectName.sln /target:Clean /v:quiet
+		msbuild $solutionFile /target:Clean /v:quiet
 	}
 }
 
 task Compile -depends Clean { 
 	exec { 
-		msbuild ../../src/$projectName.sln /p:Configuration="$configuration" /v:quiet
+		msbuild $solutionFile /p:Configuration="$configuration" /v:quiet
 	}
 }
 
 task Test -depends Compile, Clean { 
 	exec {
-		../../tools/xunit-1.9.1/xunit.console.clr4.exe ../../src/$projectName.UnitTests/bin/$configuration/$projectName.UnitTests.dll /nunit results.xml
+		& $xunitToolsPath $unitTestProject /nunit results.xml
 	}
 }
 
